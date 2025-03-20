6346c139271e00b04d1afcd1b020040340e09159
akravets <aleckravets@gmail.com>
2025-03-20 18:06:43 +0300

    adding mutations with server actions

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -11,7 +11,9 @@ export default function Search({ placeholder }: { placeholder: string }) {
 
   const handleSearch = useDebouncedCallback((term: string) => {
     const params = new URLSearchParams(searchParams);
-    params.set("page", "1");
+    
+    params.delete("page");
+
     if (term) {
       params.set("query", term);
     } else {
